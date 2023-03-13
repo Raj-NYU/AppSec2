2ae25bbf5fc4f65c65c23451f87601de2755d233
Priyaj Shah <89796238+Raj-NYU@users.noreply.github.com>
2023-03-13 02:44:50 -0400

    Update XSS_attack.py

--- Part1/XSS_attack.py
+++ Part1/XSS_attack.py
@@ -1,1 +1,14 @@
+import requests
 
+class Part1Tests(TestCase):
+    def test_xss_vulnerability(self):
+        #Change URL if needed to for more tests
+        URL = 'http://127.0.0.1:8000/gift.html'
+        session = requests.Session()
+        params = {'director': '<script>alert("{alert_string}")</script>'}
+        response = session.get(URL, params=params)
+
+        if response.text.find("<script>alert\('{alert_string}'\)</script>") != -1:
+            print("XSS vulnerability detected")
+        else:
+            print("SAFE!!!")
