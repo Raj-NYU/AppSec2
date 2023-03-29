9bb208ba475d574e7f3735fdf54c46c955c1fe03
Priyaj Shah <89796238+Raj-NYU@users.noreply.github.com>
2023-03-29 14:25:26 -0400

    part_1_complete
    updating test cases for testing

--- part1/Test_Case(s)/Django_TestCases.py
+++ part1/Test_Case(s)/Django_TestCases.py
@@ -1,62 +1,36 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 import requests
 
-#XSS
+# XSS
 class Part1Tests(TestCase):
     def test_xss_vulnerability(self):
-        URL = 'http://127.0.0.1:8000/gift.html'
-        session = requests.Session()
-        params = {'director': '<script>alert("{alert_string}")</script>'}
-        response = session.get(URL, params=params)
+        client = Client()
+        alert_string = "alert_string"
+        response = client.get('/gift.html?director=<script>alert("{0}")</script>'.format(alert_string))
+        self.assertNotContains(response, alert_string)
 
-        if response.text.find("<script>alert\('{alert_string}'\)</script>") != -1:
-            print("XSS vulnerability detected!!!")
-        else:
-            print("SAFE!!!")
-
-#CSRF
+# CSRF
 class TestCSRFExploit(TestCase):
     def test_csrf_exploit(self):
-        session = requests.Session()
-        cookies = requests.cookies.RequestsCookieJar()
-        URL = 'http://127.0.0.1:8000/gift/0'
-        data = {'username': 'hacker', 'password': 'hacker'}
-        session.post(URL, data=data, cookies=cookies)
-        body_text = session.get(URL, cookies=cookies).text
+        client = Client(enforce_csrf_checks=True)
+        response = client.post('/gift/0', {'username': 'hacker', 'password': 'hacker'}, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEqual(response.status_code, 403)
 
-        if "hacker" in body_text:
-            print("CSRF vulnerability detected!!!")
-        else:
-            print("SAFE!!!")
-
-#SQLi
+# SQLi
 class SQLInjectionTest(TestCase):
     def test_sql_injection(self):
-        file = open('part1/Attack_Cases/sqli.gftcrd')
-        URL = 'http://127.0.0.1:8000/use.html'
-        session = requests.Session()
-        body = session.post(URL, data=file)
+        client = Client()
         card_key = ""
-        if body.text.find(card_key):
-            print("SQL Injection vulnerability detected!!!")
-        else:
-            print("SAFE!!!")
+        with open('part1/Attack_Cases/sqli.gftcrd', 'r') as file:
+            response = client.post('/use.html', {'file': file})
+            self.assertNotContains(response, card_key)
 
-#Random Exploit - OS Command Injection
+# OS Command Injection
 class CommandInjectionTestCase(TestCase):
     def test_command_injection(self):
-        URL = 'http://127.0.0.1:8000/use.html'
+        client = Client()
         malicious_command = "gift;ifconfig;ls;"
-        card_key = ''
-        session = requests.Session()
-        body = session.post(URL, data=malicious_command)
-
-        try:
-            with open('part1/Attack_Cases/dummy.gftcrd') as file:
-                session.post(URL, data=file)
-                if body.text.find(card_key):
-                    print("OS Command Injection vulnerability detected!!!")
-                else:
-                    print("SAFE!!!")
-        except:
-            pass
+        card_key = ""
+        with open('part1/Attack_Cases/dummy.gftcrd', 'r') as file:
+            response = client.post('/use.html', {'file': file, 'command': malicious_command})
+            self.assertNotContains(response, card_key)
